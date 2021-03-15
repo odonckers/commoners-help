6c2e5724748a8e9a7269315f5a867116a1e71bf4
Owen Donckers <odonckers@gmail.com>
2021-03-14 23:01:39 -0500

    Added React Bootstrap

--- site/src/pages/Home/Home.tsx
+++ site/src/pages/Home/Home.tsx
@@ -1,4 +1,6 @@
 import React, { FunctionComponent } from "react"
+import Container from "react-bootstrap/Container"
+import Navbar from "react-bootstrap/Navbar"
 import { Link, withRouter } from "react-router-dom"
 import styles from "./Home.module.css"
 
@@ -6,45 +8,50 @@ type HomeProps = {}
 
 const Home: FunctionComponent<HomeProps> = (props) => {
   return (
-    <div className={`${styles.container} animateFadeIn`}>
-      <div className={styles.containerInner}>
-        {/* Hero Artwork */}
+    <Container>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      </Navbar>
+      <div className={`${styles.container} animateFadeIn`}>
+        <div className={styles.containerInner}>
+          {/* Hero Artwork */}
 
-        <div className={`${styles.heroArtwork} animateFlicker`}>
-          <img
-            draggable="false"
-            src={"./fullstack-app-artwork.png"}
-            alt="serverless-fullstack-application"
-          />
-        </div>
-        <div className={`${styles.heroTitle}`}>
-          <img
-            draggable="false"
-            src={"./fullstack-app-title.png"}
-            alt="serverless-fullstack-application"
-          />
-        </div>
+          <div className={`${styles.heroArtwork} animateFlicker`}>
+            <img
+              draggable="false"
+              src={"./fullstack-app-artwork.png"}
+              alt="serverless-fullstack-application"
+            />
+          </div>
+          <div className={`${styles.heroTitle}`}>
+            <img
+              draggable="false"
+              src={"./fullstack-app-title.png"}
+              alt="serverless-fullstack-application"
+            />
+          </div>
 
-        {/* Hero Description */}
+          {/* Hero Description */}
 
-        <div className={`${styles.heroDescription}`}>
-          A serverless full-stack application built with AWS Lambda, AWS HTTP
-          API, Express.js, React & AWS DynamoDB.
-        </div>
+          <div className={`${styles.heroDescription}`}>
+            A serverless full-stack application built with AWS Lambda, AWS HTTP
+            API, Express.js, React & AWS DynamoDB.
+          </div>
 
-        {/* Call To Action */}
+          {/* Call To Action */}
 
-        <div className={`${styles.containerCta}`}>
-          <Link to="/register">
-            <button className={`buttonPrimaryLarge`}>Register</button>
-          </Link>
+          <div className={`${styles.containerCta}`}>
+            <Link to="/register">
+              <button className={`buttonPrimaryLarge`}>Register</button>
+            </Link>
 
-          <Link to="/login" className={`${styles.linkSignIn}`}>
-            sign-in
-          </Link>
+            <Link to="/login" className={`${styles.linkSignIn}`}>
+              sign-in
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </Container>
   )
 }
 
